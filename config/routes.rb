7bae88bc595e3a36ed7b85de9f97a25902077822
Anthony Underwood <email2ants@gmail.com>
2012-09-04 23:41:51 +0100

    add sorcery simple authentication first steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Announca::Application.routes.draw do
+  get "user_sessions/new"
+
+  get "user_sessions/create"
+
+  get "user_sessions/destroy"
+
+  resources :users
+  resources :user_sessions
+
+  match 'login' => 'user_sessions#new', :as => :login
+  match 'logout' => 'user_sessions#destroy', :as => :logout
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
