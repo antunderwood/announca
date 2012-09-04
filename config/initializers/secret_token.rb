55b9334655741a91c35b0facf4a8cecbf12b5213
Anthony Underwood <email2ants@gmail.com>
2012-09-04 21:54:05 +0100

    initial setup and sorcery, bootstrap gem installs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Announca::Application.config.secret_token = '7e486911964f79d885831774036ece8698d457b0c2cb7fbb5bf49e848cc076c5229ed569f336f62664a0ba8899c360718d1ea6b3da63f5c509f2b3b016323e66'
