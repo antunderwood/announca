77f9896a84d230139011d9faaab01c0c78db5649
Anthony Underwood <email2ants@gmail.com>
2012-10-01 00:03:52 +0100

    remove unecessary method

--- app/controllers/announcees_controller.rb
+++ app/controllers/announcees_controller.rb
@@ -82,12 +82,4 @@ class AnnounceesController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
-  def unsubscribe
-    announcee = Announcee.find_by_token(params[:token])
-    group = Group.find_by_name(params[:group_name])
-    logger.info group.inspect
-    GroupMembership.find_by_announcee_id_and_group_id(announcee.id, group.id).update_attribute(:subscribed_to_mailings, false)
-    redirect_to posts_url(:token => announcee.token), notice: "Unsubscribed to mailings from the #{group.name} group"
-  end
 end
